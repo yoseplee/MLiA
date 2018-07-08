6fc4c732a0545c6ff7fd5a6752500c8646e6b97f
yoseplee <joseph38.local@gmail.com>
2018-07-08 20:52:21 +0900

    Naive base training function

--- naivebayes/bayes.py
+++ naivebayes/bayes.py
@@ -1,4 +1,5 @@
 # 2018.07.07 Naive bayes
+from numpy import *
 
 def loadDataSet():
     postingList = [
@@ -25,3 +26,20 @@ def setOfWords2Vec(vocabList, inputSet):
             returnVec[vocabList.index(word)] = 1
         else : print "the word: %s is not in my Vocabulary!" % word
     return returnVec
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory)/float(numTrainDocs)
+    p0Num = zeros(numWords); p1Num = zeros(numWords)
+    p0Denom = 0.0; p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else :
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = p1Num/p1Denom      #change to log()
+    p0Vect = p0Num/p0Denom      #change to log()
+    return p0Vect, p1Vect, pAbusive
